f069e845189610823a76df4bc70af635e16c7a61
Bagus Bayu Sasongko <18104005@ittelkom-pwt.ac.id>
2020-07-08 17:57:15 +0700

    Create class to connect Mysql

---
+++ src/system/Database.java
@@ -0,0 +1,47 @@
+package system;
+
+import javax.swing.*;
+import java.sql.*;
+
+public class Database {
+    private String host = "jdbc:mysql://localhost:3306/casher_geprek";
+    private String user = "root";
+    private String pass = "";
+    private Statement statement;
+    private Connection connection;
+    private ResultSet resultSet;
+
+    //Constructor and Connecting to Database
+    public Database(){
+        try {
+            connection = DriverManager.getConnection(host, user, pass);
+            statement = connection.createStatement();
+            System.out.println("Database Connected");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            JOptionPane.showMessageDialog(null,""+throwables.getMessage(),
+                    "Database Connect Error", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+    // Get Data from Database
+    public ResultSet getData(String query){
+        try {
+            resultSet = statement.executeQuery(query);
+        }catch (SQLException throwables){
+            JOptionPane.showMessageDialog(null,""+throwables.getMessage(),
+                    "\nFailed to Get Data", JOptionPane.WARNING_MESSAGE);
+        }
+        return resultSet;
+    }
+
+    // Execute query data
+    public void setData(String query){
+        try{
+            statement.executeQuery(query);
+        }catch (SQLException throwables){
+            JOptionPane.showMessageDialog(null,"Error" + throwables.getMessage(),
+                    "\nFailed to upload Query! ", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+}
